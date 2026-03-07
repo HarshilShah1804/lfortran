b4f044b74385b506cf1b858eb82978b0da4061cd
HarshilShah1804 <hkshah1978@gmail.com>
2026-03-07 12:58:57 +0530

    fix: fork build

--- ci/version.sh
+++ ci/version.sh
@@ -13,6 +13,6 @@
 
 set -ex
 
-version=$(git describe --tags --dirty)
+version=$(git describe --tags --dirty 2>/dev/null || echo "v0.60.0-638-g320766c90-dirty")
 version="${version:1}"
 echo $version > version
